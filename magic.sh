966c5577ae7f37b7e658d154d36b4530069764e8
CodeWebToken <94777194+CodeWebToken@users.noreply.github.com>
2024-06-16 12:46:11 -0600

    Update magic.sh - Adding hashes into file to be used on the blockchain as needed

--- magic.sh
+++ magic.sh
@@ -7,24 +7,33 @@ mkdir -p "$hooks_dir"
 pre_push_hook="$hooks_dir/pre-push"
 touch "$pre_push_hook"
 
-echo '#!/bin/bash' > "$pre_push_hook"
-echo 'ipfs_hash=$(ipfs add -r . --quieter)' >> "$pre_push_hook"
-echo 'echo "Added files to IPFS. IPFS hash: $ipfs_hash"' >> "$pre_push_hook"
+cat << 'EOF' > "$pre_push_hook"
+#!/bin/bash
+ipfs_hash=$(ipfs add -r . --quieter)
+echo "Added files to IPFS. IPFS hash: $ipfs_hash"
+echo $ipfs_hash >> .ipfs_hashes
+EOF
 
 chmod +x "$pre_push_hook"
 
+echo ".ipfs_hashes" >> .gitignore
 echo ".gitx" >> .gitignore
 
 git_add_script=".gitx"
 touch "$git_add_script"
 
-echo '#!/bin/bash' > "$git_add_script"
-echo 'ipfs_hash=$(ipfs add -r . --quieter)' >> "$git_add_script"
-echo 'echo "Added files to IPFS. IPFS hash: $ipfs_hash"' >> "$git_add_script"
+cat << 'EOF' > "$git_add_script"
+#!/bin/bash
+ipfs_hash=$(ipfs add -r . --quieter)
+echo "Added files to IPFS. IPFS hash: $ipfs_hash"
+echo $ipfs_hash >> .ipfs_hashes
+EOF
 
 chmod +x "$git_add_script"
 
-chflags hidden "$git_add_script"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    chflags hidden "$git_add_script"
+fi
 
 echo "Successfully used magic :) - Will proceed to delete magic.sh now"
 
